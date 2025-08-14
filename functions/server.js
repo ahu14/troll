949e69900055302d8d49be4014398686a68d8b9c
Sherly Hu <sherlyhu28@gmail.com>
2025-08-14 16:58:48 +0700

    revert to the last one

--- functions/server.js
+++ functions/server.js
@@ -9,5 +9,9 @@ router.get('/', (req, res) => {
     res.send("index.html");
 });
 
+<<<<<<< HEAD:netlify/functions/server.js
 app.use("/.netlify/functions/server", router);
 module.exports.handler  = serverless(app);
+=======
+export const handler = serverless(app);
+>>>>>>> parent of a211401 (change the export to module.exports):functions/server.js
